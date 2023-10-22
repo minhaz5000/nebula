3279cf1753f25540c4f0aa41323f05033048eb52
Minhazul Haque <minhaz5000@gmail.com>
2023-10-22 12:50:39 +0600

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    // Initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL Window", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        // Render here
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    return 0;
+}
